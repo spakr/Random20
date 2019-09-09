0b44bf853d3ad92291fccbee2e89e170e53c5783
spakr <43635221+spakr@users.noreply.github.com>
2019-09-09 11:20:09 +0200

    Add files via upload

---
+++ random3.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class random3 {
+    public static void main(String[] args) {
+        int size = 20;
+
+        ArrayList<Integer> list = new ArrayList<Integer>(size);
+        for(int i = 1; i <= size; i++) {
+            list.add(i);
+        }
+
+        Random rand = new Random();
+        while(list.size() > 0) {
+            int index = rand.nextInt(list.size());
+            System.out.println(+list.remove(index));
+        }
+        {
+
+        }
+    }
+}
